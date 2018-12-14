17fb9b5a0fc14fcf70200f888d7daabeedd26fde
Caner Akdaş <canerakdas@gmail.com>
2018-12-14 15:35:14 +0300

    Settings file read write added

--- main.go
+++ main.go
@@ -1,16 +1,76 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
-	"log"
-	"os/exec"
+	"io/ioutil"
+	"os"
+	"path/filepath"
 )
 
+type Settings struct {
+	UserName     string   `json:"user.name"`
+	UserEmail    string   `json:"user.email"`
+	Repositories []string `json:"repositories"`
+}
+
 func main() {
-	cmd := exec.Command("git", "status")
-	out, err := cmd.CombinedOutput()
+	// Read the settings file
+
+	var settingsPath = "/tmp/gogit/settings.json"
+	settingsFile, err := os.Open(settingsPath)
+
 	if err != nil {
-		log.Fatalf("go build failed with %s\n", err)
+		newpath := filepath.Join("/", "tmp", "gogit")
+		os.MkdirAll(newpath, os.ModePerm)
+		fmt.Println("Settings file not found, settings file created")
+		settingsFile, err = os.Create(settingsPath)
+
+		if err != nil {
+			fmt.Println("Settings file can't added :(", err)
+		}
+
+		settingsFile, err = os.Open(settingsPath)
+
+		if err != nil {
+			fmt.Print("Whoops")
+		}
+		byteValue, _ := ioutil.ReadAll(settingsFile)
+
+		var result map[string]interface{}
+		json.Unmarshal([]byte(byteValue), &result)
+
+		fmt.Println(result["repositories"])
+		fmt.Println(result["user.name"])
 	}
-	fmt.Printf("%s\n", string(out))
+
+	byteValue, _ := ioutil.ReadAll(settingsFile)
+
+	var result map[string]interface{}
+	json.Unmarshal([]byte(byteValue), &result)
+
+	fmt.Println(result["repositories"])
+	fmt.Println(result["user.name"])
+
+	settings := Settings{
+		UserName:     "caner",
+		UserEmail:    "gmail.com",
+		Repositories: []string{"hello"},
+	}
+
+	reqBodyBytes := new(bytes.Buffer)
+	json.NewEncoder(reqBodyBytes).Encode(settings)
+
+	err = json.Unmarshal(reqBodyBytes.Bytes(), &settings)
+	if err != nil {
+		fmt.Println(err)
+		// nozzle.printError("opening config file", err.Error())
+	}
+
+	rankingsJson, _ := json.Marshal(settings)
+	err = ioutil.WriteFile(settingsPath, rankingsJson, 0777)
+	fmt.Printf("%+v", settings)
+	// defer the closing of our jsonFile so that we can parse it later on
+	defer settingsFile.Close()
 }
