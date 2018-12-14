218143daa018d722d2930983f37614cb0adfd160
Segmentify <segmentify@MacBook-Pro-3.local>
2018-12-14 16:25:26 +0300

    Git status for all

--- main2.go
+++ main2.go
@@ -5,11 +5,22 @@ import (
 	"log"
 	"os/exec"
 	"strings"
+
+	"github.com/fatih/color"
 )
 
+var repostories []string
+
 func main() {
-	cmd := exec.Command("git", "status", "-s")
-	out, err := cmd.CombinedOutput()
+	repostories = append(repostories, "/Users/segmentify/accounts")
+	repostories = append(repostories, "/Users/segmentify/go/src/github.com/canerakdas/gogit")
+	for i := 0; i < len(repostories); i++ {
+		goGitStatus(repostories[i])
+	}
+}
+func goGitStatus(repo string) {
+	cmd := exec.Command("git", "-C", repo, "status", "-s")
+	out, err := cmd.Output()
 	if err != nil {
 		log.Fatalf("go build failed with %s\n", err)
 	}
@@ -23,9 +34,19 @@ func main() {
 			newFileArray = append(newFileArray, lines[i])
 		}
 	}
-	fmt.Println("Modified Files : ")
-	fmt.Println(modifiedArray)
+	color.Blue("\n Repo : %s \n\n", repo)
+	if len(modifiedArray) != 0 {
+		fmt.Println("Modified Files")
+		for i := 0; i < len(modifiedArray); i++ {
+			color.Yellow("  ->  %s", strings.Fields(modifiedArray[i])[1])
+		}
+	}
 
-	fmt.Println("New Files : ")
-	fmt.Println(newFileArray)
+	if len(newFileArray) != 0 {
+		fmt.Println("New Files")
+		for i := 0; i < len(newFileArray); i++ {
+			color.Green("  ->  %s", strings.Fields(newFileArray[i])[1])
+		}
+	}
+	color.White("\n--------------------------------- \n\n")
 }
