999a795e2fc9e452d85ff19b591a5965914c7bf2
Caner Akdaş <canerakdas@gmail.com>
2018-12-14 23:17:17 +0300

    main.go updated, basic methods and flags added

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"encoding/json"
+	"flag"
 	"fmt"
 	"io/ioutil"
 	"os"
@@ -10,67 +11,136 @@ import (
 )
 
 type Settings struct {
-	UserName     string   `json:"user.name"`
-	UserEmail    string   `json:"user.email"`
-	Repositories []string `json:"repositories"`
+	User         User         `json:"user"`
+	Repositories Repositories `json:"repositories"`
+}
+
+type User struct {
+	Name  string `json:"name"`
+	Email string `json:"email"`
+}
+
+type Repositories struct {
+	List []string `json:"repositories"`
+}
+
+var CurrentSettings Settings
+var settingsPath = "/tmp/gogit/settings.json"
+
+func (s Settings) UpdateEmail(v string) {
+	s.User.Email = v
+	s.WriteSettingsFile()
+}
+
+func (s Settings) UpdateName(v string) {
+	s.User.Name = v
+	s.WriteSettingsFile()
+}
+
+func (s Settings) AddRepositories(v string) {
+	var hasPath = false
+	for _, t := range s.Repositories.List {
+		if t == v {
+			hasPath = true
+		}
+	}
+	if hasPath != true {
+		s.Repositories.List = append(s.Repositories.List, v)
+		s.WriteSettingsFile()
+	} else {
+		fmt.Println("This repo already added")
+	}
+}
+
+func (s Settings) RemoveRepositories(v string) {
+	var pathIndex = -1
+	for i, t := range s.Repositories.List {
+		if t == v {
+			pathIndex = i
+		}
+	}
+	if pathIndex != -1 {
+		s.Repositories.List = append(s.Repositories.List[:pathIndex], s.Repositories.List[pathIndex+1:]...)
+		s.WriteSettingsFile()
+	} else {
+		fmt.Println("This repo already deleted")
+	}
+}
+
+func (s Settings) WriteSettingsFile() {
+	reqBodyBytes := new(bytes.Buffer)
+	json.NewEncoder(reqBodyBytes).Encode(s)
+
+	err := json.Unmarshal(reqBodyBytes.Bytes(), &s)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	emptyJSON, _ := json.Marshal(s)
+	err = ioutil.WriteFile(settingsPath, emptyJSON, 0777)
 }
 
 func main() {
 	// Read the settings file
-
-	var settingsPath = "/tmp/gogit/settings.json"
 	settingsFile, err := os.Open(settingsPath)
 
 	if err != nil {
-		newpath := filepath.Join("/", "tmp", "gogit")
-		os.MkdirAll(newpath, os.ModePerm)
-		fmt.Println("Settings file not found, settings file created")
+		fmt.Println("Settings file could not found.")
+
+		// Create the gogit directory
+		gogitPath := filepath.Join("/", "tmp", "gogit")
+		os.MkdirAll(gogitPath, os.ModePerm)
 		settingsFile, err = os.Create(settingsPath)
+		fmt.Println("Settings folder created.")
 
 		if err != nil {
-			fmt.Println("Settings file can't added :(", err)
+			fmt.Println("Something went wrong, settings folder could not created.", err)
 		}
 
-		settingsFile, err = os.Open(settingsPath)
+		settingsBodyBytes := new(bytes.Buffer)
+		json.NewEncoder(settingsBodyBytes).Encode(CurrentSettings)
+
+		err = json.Unmarshal(settingsBodyBytes.Bytes(), &CurrentSettings)
 
 		if err != nil {
-			fmt.Print("Whoops")
+			fmt.Println("Something went wrong, settings file could not created.", err)
 		}
-		byteValue, _ := ioutil.ReadAll(settingsFile)
+
+		settingsJSON, _ := json.Marshal(CurrentSettings)
+		err = ioutil.WriteFile(settingsPath, settingsJSON, 0777)
+	} else {
+		// Read current settings
+		settingsByteValue, _ := ioutil.ReadAll(settingsFile)
 
 		var result map[string]interface{}
-		json.Unmarshal([]byte(byteValue), &result)
+		json.Unmarshal([]byte(settingsByteValue), &result)
 
-		fmt.Println(result["repositories"])
-		fmt.Println(result["user.name"])
+		json.Unmarshal(settingsByteValue, &CurrentSettings)
+
+		// Set flags
+		emailFlag := flag.String("e", "", "GitHub User Email")
+		nameFlag := flag.String("n", "", "GitHub User Name")
+		repostoriesAddFlag := flag.String("a", "", "Add the new directory")
+		repostoriesRemoveFlag := flag.String("r", "", "Remove the directory")
+		listFlag := flag.String("l", "s", "List the current settings")
+
+		flag.Parse()
+		if *emailFlag != "" {
+			CurrentSettings.UpdateEmail(*emailFlag)
+		}
+
+		if *nameFlag != "" {
+			CurrentSettings.UpdateName(*nameFlag)
+		}
+		if *repostoriesAddFlag != "" {
+			CurrentSettings.AddRepositories(*repostoriesAddFlag)
+		}
+		if *repostoriesRemoveFlag != "" {
+			CurrentSettings.RemoveRepositories(*repostoriesRemoveFlag)
+		}
+		if *listFlag == "s" {
+			fmt.Println(CurrentSettings)
+		}
 	}
-
-	byteValue, _ := ioutil.ReadAll(settingsFile)
-
-	var result map[string]interface{}
-	json.Unmarshal([]byte(byteValue), &result)
-
-	fmt.Println(result["repositories"])
-	fmt.Println(result["user.name"])
-
-	settings := Settings{
-		UserName:     "caner",
-		UserEmail:    "gmail.com",
-		Repositories: []string{"hello"},
-	}
-
-	reqBodyBytes := new(bytes.Buffer)
-	json.NewEncoder(reqBodyBytes).Encode(settings)
-
-	err = json.Unmarshal(reqBodyBytes.Bytes(), &settings)
-	if err != nil {
-		fmt.Println(err)
-		// nozzle.printError("opening config file", err.Error())
-	}
-
-	rankingsJson, _ := json.Marshal(settings)
-	err = ioutil.WriteFile(settingsPath, rankingsJson, 0777)
-	fmt.Printf("%+v", settings)
-	// defer the closing of our jsonFile so that we can parse it later on
 	defer settingsFile.Close()
 }
